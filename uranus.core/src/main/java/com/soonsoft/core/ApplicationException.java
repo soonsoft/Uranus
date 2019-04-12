63295bb5336f6501b7b77f88052bfe967b0718f0
soonsoft <soonz@outlook.com>
2019-04-12 15:15:33 +0800

    创建工程

---
+++ uranus.core/src/main/java/com/soonsoft/core/ApplicationException.java
@@ -0,0 +1,11 @@
+package com.soonsoft.core;
+
+/**
+ * ApplicationException
+ */
+public class ApplicationException extends Exception {
+
+    private static final long serialVersionUID = 9060800082887148768L;
+
+    
+}
