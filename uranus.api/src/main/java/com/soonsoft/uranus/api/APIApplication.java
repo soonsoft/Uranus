f983b0bba51b9be9fb42dd4e10a66aea3a3b0158
soonsoft <soonz@outlook.com>
2020-12-02 18:20:25 +0800

    准备JWT框架

--- uranus.api/src/main/java/com/soonsoft/uranus/api/APIApplication.java
+++ uranus.api/src/main/java/com/soonsoft/uranus/api/APIApplication.java
@@ -23,6 +23,14 @@ public class ApiApplication {
     }
 
     private static void onStop() {
+        /*
+            1. 程序正常退出
+            2. 使用System.exit()
+            3. 终端使用Ctrl+C触发的中断
+            4. 系统关闭
+            5. OutOfMemory宕机
+            6. 使用Kill pid命令干掉进程（注：在使用kill -9 pid时，是不会被调用的）
+        */
         Runtime.getRuntime().addShutdownHook(new Thread() {
             @Override
             public void run() {
