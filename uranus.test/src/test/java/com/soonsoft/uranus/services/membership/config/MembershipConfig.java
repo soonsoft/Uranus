2dc9728c97a52bcba0f001cf9442f3c782c99507
soonsoft <soonz@outlook.com>
2021-12-31 15:41:37 +0800

    权限校验支持“特权（privilege）”模型

--- uranus.test/src/test/java/com/soonsoft/uranus/services/membership/config/MembershipConfig.java
+++ uranus.test/src/test/java/com/soonsoft/uranus/services/membership/config/MembershipConfig.java
@@ -5,17 +5,6 @@ import javax.sql.DataSource;
 import com.soonsoft.uranus.data.EnableDatabaseAccess;
 import com.soonsoft.uranus.data.config.DataSourceFactory;
 import com.soonsoft.uranus.data.IDatabaseAccess;
-import com.soonsoft.uranus.services.membership.service.FunctionService;
-import com.soonsoft.uranus.services.membership.service.RoleService;
-import com.soonsoft.uranus.services.membership.service.UserService;
-import com.soonsoft.uranus.services.membership.dao.AuthPasswordDAO;
-import com.soonsoft.uranus.services.membership.dao.AuthRoleDAO;
-import com.soonsoft.uranus.services.membership.dao.AuthPermissionDAO;
-import com.soonsoft.uranus.services.membership.dao.AuthPrivilegeDAO;
-import com.soonsoft.uranus.services.membership.dao.AuthUserDAO;
-import com.soonsoft.uranus.services.membership.dao.AuthUserRoleRelationDAO;
-import com.soonsoft.uranus.services.membership.dao.SysFunctionDAO;
-import com.soonsoft.uranus.services.membership.dao.SysMenuDAO;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringBootConfiguration;
@@ -41,38 +30,18 @@ public class MembershipConfig {
     }
 
     @Bean
-    public UserService userService(@Qualifier("membershipAccess") IDatabaseAccess<?> securityAccess, PasswordEncoder passwordEncoder) {
-        AuthUserDAO authUserDAO = new AuthUserDAO(securityAccess);
-        AuthPasswordDAO authPasswordDAO = new AuthPasswordDAO(securityAccess);
-        AuthUserRoleRelationDAO userRoleRelationDAO = new AuthUserRoleRelationDAO(securityAccess);
-        AuthPrivilegeDAO privilegeDAO = new AuthPrivilegeDAO(securityAccess);
-
-        UserService userService = new UserService(authUserDAO, authPasswordDAO, userRoleRelationDAO, privilegeDAO);
-        userService.setPasswordEncoder(passwordEncoder);
-
-        return userService;
+    public UserServiceFactory userServiceFactory(@Qualifier("membershipAccess") IDatabaseAccess<?> securityAccess, PasswordEncoder passwordEncoder) {
+        return new UserServiceFactory(securityAccess, passwordEncoder);
     }
 
     @Bean("membershipRoleService")
-    public RoleService roleService(@Qualifier("membershipAccess") IDatabaseAccess<?> securityAccess) {
-        AuthRoleDAO roleDAO = new AuthRoleDAO(securityAccess);
-        AuthUserRoleRelationDAO userRoleRelationDAO = new AuthUserRoleRelationDAO(securityAccess);
-        AuthPermissionDAO permissionDAO = new AuthPermissionDAO(securityAccess);
-        
-        RoleService roleService = new RoleService(roleDAO, userRoleRelationDAO, permissionDAO);
-
-        return roleService;
+    public RoleServiceFactory roleServiceFactory(@Qualifier("membershipAccess") IDatabaseAccess<?> securityAccess) {
+        return new RoleServiceFactory(securityAccess);
     }
 
     @Bean
-    public FunctionService functionService(@Qualifier("membershipAccess") IDatabaseAccess<?> securityAccess) {
-        SysFunctionDAO functionDAO = new SysFunctionDAO(securityAccess);
-        SysMenuDAO menuDAO = new SysMenuDAO(securityAccess);
-        AuthPermissionDAO permissionDAO = new AuthPermissionDAO(securityAccess);
-
-        FunctionService functionService = new FunctionService(functionDAO, menuDAO, permissionDAO);
-        
-        return functionService;
+    public FunctionServiceFactory functionServiceFactory(@Qualifier("membershipAccess") IDatabaseAccess<?> securityAccess) {
+        return new FunctionServiceFactory(securityAccess);
     }
 
 }
