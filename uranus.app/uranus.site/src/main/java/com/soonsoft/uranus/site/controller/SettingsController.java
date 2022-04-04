02e64c549f4a281498683b1af39ed653860dd94d
SoonSoft <soonz@outlook.com>
2022-04-04 18:28:23 +0800

    调整接口

--- uranus.app/uranus.site/src/main/java/com/soonsoft/uranus/site/controller/SettingsController.java
+++ uranus.app/uranus.site/src/main/java/com/soonsoft/uranus/site/controller/SettingsController.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import com.soonsoft.uranus.data.entity.Page;
