d5447eaca12fcfa8ae4e5e5082e72bc6dbfebc53
SoonSoft <soonz@outlook.com>
2024-02-22 20:04:50 +0800

    update springboot version to 2.7.18

--- uranus.test/src/main/java/com/soonsoft/uranus/test/Test.java
+++ uranus.test/src/main/java/com/soonsoft/uranus/test/Test.java
@@ -1,9 +1,16 @@
 package com.soonsoft.uranus.test;
 
-/**
- * from spring
- */
+import com.soonsoft.uranus.core.functional.func.Func2;
+
 public class Test {
 
+    public static void main(String[] args) {
+        Func2<Integer, Integer, Integer> func = (a, b) -> a + b;
+        int result = func.call(1, 2);
+        System.out.println(result == 3);
+
+        Object obj = func.call(5, 5);
+        System.out.println(obj instanceof Integer);
+    }
     
 }
